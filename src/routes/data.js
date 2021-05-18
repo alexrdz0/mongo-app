45dd6f59fb34c80971f63bcd1c48f003ccd89ab7
Alonso Salcido <alonsosalcido13@gmail.com>
2021-05-18 15:11:18 -0600

    New Function to Search Data
    Adding Search2 function to search data by the condition of the req.query exist or not.

--- src/routes/data.js
+++ src/routes/data.js
@@ -149,6 +149,71 @@ router.delete('/data/delete/:id',async (req, res) =>{
     res.redirect('/data');
 });
 */
+//BUSCAR2
+router.get('/data/search2', /*isLoggedIn,*/ async function (req, res) {
+    //buscar
+    var noMatch = null;
+    //const errors = [];
+    if (req.query.search) {
+        console.log("Se esta buscando: ", req.query.search);
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        console.log(regex)
+        //consultar base de datos
+        console.log("pasando el regex");
+        const data = await Data.findOne({nuc: req.query.search})
+        console.log(data)
+        res.render('data/all-data.hbs', { datos:  data})
+    }else {
+        //consultar base de datos
+        console.log("ingresando al else");
+        await Data.find().sort({ date: 'desc' })
+            .then(documentos => {
+                const contexto = {
+                    datos: documentos.map(documento => {
+                        return {
+                            _id: documento._id,
+                            departamento: documento.departamento,
+                            avance: documento.avance,
+                            status: documento.status,
+                            nuc: documento.nuc,
+                            oficio: documento.oficio,
+                            equipo: documento.equipo,
+                            unidad: documento.unidad,
+                            zona: documento.zona,
+                            fechaD: documento.fechaD,
+                            hora: documento.hora,
+                            fechaR: documento.fechaR,
+                            id_del: documento.id_del,
+                            delito: documento.delito,
+                            num_if: documento.num_if,
+                            fechaC: documento.fechaC,
+                            lic: documento.lic,
+                            agente: documento.agente,
+                            fechaRecol: documento.fechaRecol,
+                            dir: documento.dir,
+                            dis: documento.dis,
+                            evidencia: documento.evidencia,
+                            banco: documento.banco,
+                            marcaEqui: documento.marcaEqui,
+                            modeloEqui: documento.modeloEqui,
+                            serieEqui: documento.serieEqui,
+                            marcaAlma: documento.marcaAlma,
+                            modeloAlma: documento.modeloAlma,
+                            serieAlma: documento.serieAlma,
+                            md5: documento.md5,
+                            sha1: documento.sha1,
+                            swImagen: documento.swImagen,
+                            swArte: documento.swArte,
+                            swInfo: documento.swInfo,
+                            file: documento.file
+                        }
+                    })
+
+                }
+            res.render('data/search', { datos: contexto.datos });
+        })
+    }
+});
 
 //BUSCAR
 router.get('/data/search', async function (req, res) {
